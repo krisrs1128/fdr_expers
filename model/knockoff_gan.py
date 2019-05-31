c7ec4b4ebbe74e578cd0acf2815813aa768d2512
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-31 11:39:55 -0400

    just usual forwards MLP

---
+++ model/knockoff_gan.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Implements the Baseline Knockoff Gan
+"""
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Generator(nn.Module):
+    def __init__(self, K=4, H=8, J=10):
+        super(Generator, self).__init__()
+        self.J = J
+        self.layers = nn.Sequential(
+            nn.Linear(K, H),
+            nn.ReLU,
+            nn.Linear(H, H),
+            nn.ReLU,
+            nn.Linear(H, J)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, J=10, H = 5):
+        super(Discriminator, self).__init__()
+        self.J = J
+        self.layers = nn.Sequential(
+            nn.Linear(J, H),
+            nn.ReLU,
+            nn.Linear(H, H),
+            nn.ReLU,
+            nn.Linear(H, 1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
