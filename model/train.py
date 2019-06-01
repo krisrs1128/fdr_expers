0f48482ae3e75b724f04e6be8fb447b97d6c4424
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-01 10:42:14 -0400

    helpers to simplify training code

--- model/train.py
+++ model/train.py
@@ -1,11 +1,13 @@
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#!/usr/bin/env python
+import torch
 
 def train(iterator, optimizers, models, loss_fun, device=None):
     if not device:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    batch_size, J = next(iter(iterator)).shape
+    dim_z = models["G"].layers[0].in_features
+    losses = np.zeros((len(iterator.dataset), 3))
+
 
     for i, data in enumerate(iterator):
         ############################
@@ -13,27 +15,30 @@ def train(iterator, optimizers, models, loss_fun, device=None):
         ###########################
         # train with real
         models["D"].zero_grad()
-        label = torch.full((batch_size,), 1, device=device)
-
-        y_hat = models["D"](real_cpu)
-        loss = loss_fun(y_hat, label)
-        loss.backward()
+        y = torch.full((iterator.batch_size,), 1, device=device)
+        losses[i][0] = loss_backward(models["D"], loss_fun, data, y)
 
         # train with fake
-        z = torch.randn(batch_size, dim_z, device=device)
-        x_tilde = models["G"](noise)
-        label.fill_(0)
-        y_hat = netD(z.detach())
-        loss = loss_fun(y_hat, label)
-        loss.backward()
+        z = torch.randn(iterator.batch_size, dim_z, device=device)
+        x_tilde = models["G"](z)
+        losses[i][1] = loss_backward(models["D"], loss_fun, x_tilde.detach(), y.fill_(0))
         optimizers["D"].step()
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
         models["G"].zero_grad()
-        label.fill_(0)  # fake labels are real for generator cost
-        y_hat = models["D"](fake)
-        loss = loss_fun(output, label)
-        loss.backward()
+        losses[i][2] = loss_backward(models["D"], loss_fun, x_tilde, y.fill_(1))
         optimizers["G"].step()
+
+        # if i % 0 == 0:
+        print("Loss {}: {}".format(i, losses[i]))
+
+    return models, optimizers, losses
+
+
+def loss_backward(model, loss_fun, x, y):
+    y_hat = model(x)
+    loss = loss_fun(y_hat.squeeze(), y)
+    loss.backward()
+    return loss
