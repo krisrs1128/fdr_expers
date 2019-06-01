7d124b923e41a35d920c0ec35873c9e0338cc0a5
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-01 13:45:28 -0400

    save the losses

--- exper/low_dim.py
+++ exper/low_dim.py
@@ -11,30 +11,26 @@ z = np.random.normal(size=(n, 1))
 f1 = add_noise(poly([2, -2, 1]))
 f2 = add_noise(poly([-1, 0, 1, 0.1]))
 x = np.hstack([z, f1(z), f2(z), np.random.normal(size=(n, 7))])
-n_epochs = 1000
+n_epochs = 10
 
 models = {"D": Discriminator(), "G": Generator()}
-optimizers = {
-    "D": torch.optim.Adam(models["D"].parameters(), lr=1e-4, betas=(0.5, 0.999)),
-    "G": torch.optim.Adam(models["G"].parameters(), lr=1e-3, betas=(0.5, 0.999))
-}
 
 xset = XSim(x)
 iterator = DataLoader(xset, batch_size=32)
 
+# test using the extra sgd
+optimizers = {
+    "D": ExtraSGD(models["D"].parameters(), lr=1e-3),
+    "G": ExtraSGD(models["G"].parameters(), lr=1e-3)
+}
+
 losses = []
 for epoch in range(n_epochs):
     models, optimizers, _ = train(iterator, optimizers, models, loss_fun)
-    # losses_i = pd.DataFrame(losses_i)
-    # losses_i["epoch"] = epoch
-    # losses.append(losses_i)
+    losses_i = pd.DataFrame(losses_i)
+    losses_i["epoch"] = epoch
+    losses.append(losses_i)
 
 losses = pd.concat(losses)
 losses.columns = ["D0", "D1", "G", "epoch"]
 losses.to_csv("losses.csv", index=False)
-
-
-
-
-
-# test using the extra sgd
