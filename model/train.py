2177c103589a77484f040dba8dd5b9af494a0514
Kris Sankaran <sankaran.kris@gmail.com>
2019-06-01 13:44:43 -0400

    updates train to use extrapolation

--- model/train.py
+++ model/train.py
@@ -8,29 +8,39 @@ def train(iterator, optimizers, models, loss_fun, device=None):
     dim_z = models["G"].layers[0].in_features
     losses = np.zeros((len(iterator), 3))
 
+    models["G"].zero_grad()
+    models["D"].zero_grad()
 
     for i, data in enumerate(iterator):
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
         # train with real
-        models["D"].zero_grad()
         y = torch.full((iterator.batch_size,), 1, device=device)
         losses[i][0] = loss_backward(models["D"], loss_fun, data, y)
 
         # train with fake
         z = torch.randn(iterator.batch_size, dim_z, device=device)
-        z = torch.randn(1000, dim_z, device=device)
         x_tilde = models["G"](z)
         losses[i][1] = loss_backward(models["D"], loss_fun, x_tilde.detach(), y.fill_(0))
-        optimizers["D"].step()
+
+        if i % 2 == 0:
+            optimizers["D"].extrapolate()
+        else:
+            optimizers["D"].step()
+            models["D"].zero_grad()
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
-        models["G"].zero_grad()
         losses[i][2] = loss_backward(models["D"], loss_fun, x_tilde, y.fill_(1))
-        optimizers["G"].step()
+
+        if i % 2 == 0:
+            optimizers["G"].extrapolate()
+        else:
+            optimizers["G"].step()
+            models["G"].zero_grad()
+
 
         # if i % 0 == 0:
         print("Loss {}: {}".format(i, losses[i]))
